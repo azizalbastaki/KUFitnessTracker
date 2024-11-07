01757e5bec8a26b9ea9ef46ea31a03474b129ecc
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-11-07 11:44:22 +0400

    account class done

---
+++ Account.java
@@ -0,0 +1,11 @@
+public class Account {
+    private String name;
+    private double id;
+    private String password; 
+
+    public Account(String name, double id, String password) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+    }
+}
