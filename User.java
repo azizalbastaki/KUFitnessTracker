da58d55aef6d77c01d8452d3c60072b788ac320a
Abdulaziz <37148753+azizalbastaki@users.noreply.github.com>
2024-11-13 19:53:40 +0400

    User class added

---
+++ User.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class User extends Account {
+
+    public User(String name, double id, String password) {
+        super(name, id, password);
+    }
+
+    private String email;
+    private Date birthdate;
+    private int phoneNumber;
+    private List<Activity> activities = new ArrayList<>();
+    private List<Goal> goals = new ArrayList<>();
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
+
+    public List<Goal> getGoals() {
+        return goals;
+    }
+
+    public void setGoals(List<Goal> goals) {
+        this.goals = goals;
+    }
+}
