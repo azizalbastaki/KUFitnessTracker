a0d360ab857fed9033dde7b32ac444a30359755b
Abd Alrahman Ismaik <abdalrahman.ismaik@gmail.com>
2024-11-30 22:57:17 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 import java.util.*;
-import javax.swing.*;
 
-public class Main extends JFrame {
-    protected static List<User> users = new ArrayList<User>();
+public class Main {
+    private static List<User> users = new ArrayList<User>();
+    private static int idx;
+    private static boolean checked;
 
-    public static List<User> getUsers() {
-        return users;
-    }
+    public static void addUser(User user){users.add(user);}
+    public static List<User> getUsers() {return users;}
 
-    public static int idx;
-    public static boolean checked;
+    public static void setIdx(int id){idx = id;}
+    public static int getIdx(){return idx;}
+    
+    public static void setChecked(boolean ch){checked = ch;}
+    public static boolean getChecked(){return checked;}
 
     public static void main(String[] args) {
 		users = Database.loadUsers(); // No exception is propagated here
